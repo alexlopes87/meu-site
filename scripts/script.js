3f974c995fb38029fe1cddb870f91a533391b50b
alexlopes87 <172157268+alexlopes87@users.noreply.github.com>
2024-07-13 15:00:31 -0300

    update media query

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,11 @@ function showMenu() {
 function closeMenu() {
     document.getElementById("menu").style.width = "0px"
 }
+
+function changeSize() {
+    if (window.innerWidth >= 768) {
+        document.getElementById("menu").style.width = "150px"
+    } else {
+        document.getElementById("menu").style.width = "0px"
+    }
+}
